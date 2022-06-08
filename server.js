8b07a30b1f0e54d49e1fc41005fbcbe69683b941
Zachary Jones <zacharyjones@microsoft.com>
2022-06-08 14:03:39 -0700

    added index for some git practice

--- server.js
+++ server.js
@@ -5,3 +5,17 @@ console.log("Hello world.");
 
 console.log(2 + 2);
 
+
+
+//Corey's silly Function :)
+const sillyFunction = (joke) => {
+    if(joke == undefined){
+       throw Error("Give me a funny joke!")
+    }
+console.log(`Hey want to hear a joke? ${joke}`)
+}
+
+sillyFunction('What did the cat🐱 say to the mouse 🐭? \n Meouuuuussseee!😹😹😹')
+hystericalLaughing();
+
+
